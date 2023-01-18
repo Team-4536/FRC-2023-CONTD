acd6e262dcaa4247a9d8060274e2233f3be884c5
Gavriel Pappas <pappasgavriel@gmail.com>
2023-01-18 14:00:04 -0600

    fix motor inversion

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -20,10 +20,10 @@ public final class Constants {
     public static final int DRIVE_BACK_LEFT_PORT = 3;
     public static final int DRIVE_BACK_RIGHT_PORT = 4;
 
-    public static final boolean DRIVE_FRONT_LEFT_FLIPPED = true;
-    public static final boolean DRIVE_FRONT_RIGHT_FLIPPED = false;
-    public static final boolean DRIVE_BACK_LEFT_FLIPPED = true;
-    public static final boolean DRIVE_BACK_RIGHT_FLIPPED = false;
+    public static final boolean DRIVE_FRONT_LEFT_FLIPPED = false;
+    public static final boolean DRIVE_FRONT_RIGHT_FLIPPED = true;
+    public static final boolean DRIVE_BACK_LEFT_FLIPPED = false;
+    public static final boolean DRIVE_BACK_RIGHT_FLIPPED = true;
 
 
 

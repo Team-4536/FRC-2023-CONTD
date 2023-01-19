7dc50eb3c582e2b488414da5b0789baaba937251
Gavriel Pappas <pappasgavriel@gmail.com>
2023-01-18 18:55:10 -0600

    fix merge conflict

--- src/main/java/frc/robot/subsystems/DriveData.java
+++ src/main/java/frc/robot/subsystems/DriveData.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants;
 import frc.robot.controllers.PIDController;

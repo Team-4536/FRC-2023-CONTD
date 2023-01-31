842f2a680177a05d0f5d2012acafbc43b20de2aa
Gavriel Pappas <pappasgavriel@gmail.com>
2023-01-31 16:25:19 -0600

    pneumatic telemetry

--- src/main/java/frc/robot/subsystems/PneumaticData.java
+++ src/main/java/frc/robot/subsystems/PneumaticData.java
@@ -3,18 +3,18 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsControlModule;
 import frc.robot.functions.telemetryUtil;
+import frc.robot.functions.telemetryUtil.Tabs;
 
 public class PneumaticData {
     
     public PneumaticsControlModule pcm = new PneumaticsControlModule(0);
 
-    public DoubleSolenoid leftBackSol = pcm.makeDoubleSolenoid(0, 0);
-    public DoubleSolenoid rightBackSol = pcm.makeDoubleSolenoid(0, 0);
-    public DoubleSolenoid leftFrontSol = pcm.makeDoubleSolenoid(0, 0);
-    public DoubleSolenoid rightFrontSol = pcm.makeDoubleSolenoid(0, 0);
+    public DoubleSolenoid breakSolenoid = pcm.makeDoubleSolenoid(0, 0);
 
 
     public void sendTelemetry(){
+
+        telemetryUtil.put("Is Extended", breakSolenoid.get() == DoubleSolenoid.Value.kForward,Tabs.ROBOT);
 
         
     }

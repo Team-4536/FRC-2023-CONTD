78c2b9a0330d6ef3ae962ee8138bd5fbb59ced8a
Robbie <robbiemaldrich@gmail.com>
2023-02-02 17:41:26 -0600

    second master for spark max motor controllers

--- src/main/java/frc/robot/subsystems/DriveData.java
+++ src/main/java/frc/robot/subsystems/DriveData.java
@@ -1,8 +1,8 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
-import edu.wpi.first.wpilibj.Encoder;
+import com.revrobotics.*;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants;
 import frc.robot.controllers.PIDController;
@@ -11,22 +11,24 @@ public class DriveData {
 
     public PIDController pidController = new PIDController(0.08, 0.01, 0);
 
-    public WPI_VictorSPX frontLeftDrive;
-    public WPI_VictorSPX frontRightDrive;
-    public WPI_VictorSPX backLeftDrive;
-    public WPI_VictorSPX backRightDrive;
+    public CANSparkMax frontLeftDrive;
+    public CANSparkMax frontRightDrive;
+    public CANSparkMax backLeftDrive;
+    public CANSparkMax backRightDrive;
 
-    public Encoder FLEncoder;
-    public Encoder FREncoder;
-    public Encoder BLEncoder;
-    public Encoder BREncoder;
+    public RelativeEncoder FLEncoder;
+    public RelativeEncoder FREncoder;
+    public RelativeEncoder BLEncoder;
+    public RelativeEncoder BREncoder;
+
+    public SparkMaxAbsoluteEncoder cum;
 
     public DriveData() {
         
-        this.frontLeftDrive = new WPI_VictorSPX(Constants.DRIVE_FRONT_LEFT_PORT);
-        this.frontRightDrive = new WPI_VictorSPX(Constants.DRIVE_FRONT_RIGHT_PORT);
-        this.backLeftDrive = new WPI_VictorSPX(Constants.DRIVE_BACK_LEFT_PORT);
-        this.backRightDrive = new WPI_VictorSPX(Constants.DRIVE_BACK_RIGHT_PORT);
+        this.frontLeftDrive = new CANSparkMax(Constants.DRIVE_FRONT_LEFT_PORT, CANSparkMaxLowLevel.MotorType.kBrushless);
+        this.frontRightDrive = new CANSparkMax(Constants.DRIVE_FRONT_RIGHT_PORT, CANSparkMaxLowLevel.MotorType.kBrushless);
+        this.backLeftDrive = new CANSparkMax(Constants.DRIVE_BACK_LEFT_PORT, CANSparkMaxLowLevel.MotorType.kBrushless);
+        this.backRightDrive = new CANSparkMax(Constants.DRIVE_BACK_RIGHT_PORT, CANSparkMaxLowLevel.MotorType.kBrushless);
 
         this.frontLeftDrive.setInverted(Constants.DRIVE_FRONT_LEFT_FLIPPED);
         this.frontRightDrive.setInverted(Constants.DRIVE_FRONT_RIGHT_FLIPPED);
@@ -34,20 +36,25 @@ public class DriveData {
         this.backRightDrive.setInverted(Constants.DRIVE_BACK_RIGHT_FLIPPED);
 
         //ChannelA is the one with 3 wires channelB is the one with 1 wire(TRUE)
-        FLEncoder = new Encoder(6, 7);
-        FREncoder = new Encoder(4, 5);
-        BLEncoder = new Encoder(0, 1);
-        BREncoder = new Encoder(2, 3);
+        FLEncoder = frontLeftDrive.getEncoder();
+        FREncoder = frontRightDrive.getEncoder();
+        BLEncoder = backLeftDrive.getEncoder();
+        BREncoder = backRightDrive.getEncoder();
 
-        BREncoder.setReverseDirection(false);
-        FLEncoder.setReverseDirection(true);
-        BLEncoder.setReverseDirection(true);
-        FREncoder.setReverseDirection(false);
+        BREncoder.setInverted(false);
+        FLEncoder.setInverted(true);
+        BLEncoder.setInverted(true);
+        FREncoder.setInverted(false);
 
-        BREncoder.setDistancePerPulse(Constants.ENCODER_PULSE_DISTANCE);
-        BLEncoder.setDistancePerPulse(Constants.ENCODER_PULSE_DISTANCE);
-        FREncoder.setDistancePerPulse(Constants.ENCODER_PULSE_DISTANCE);
-        FLEncoder.setDistancePerPulse(Constants.ENCODER_PULSE_DISTANCE);
+        BREncoder.setPositionConversionFactor(1);
+        FLEncoder.setPositionConversionFactor(1);
+        BLEncoder.setPositionConversionFactor(1);
+        FREncoder.setPositionConversionFactor(1);
+
+        //BREncoder.setDistancePerPulse(Constants.ENCODER_PULSE_DISTANCE);
+        //BLEncoder.setDistancePerPulse(Constants.ENCODER_PULSE_DISTANCE);
+        //FREncoder.setDistancePerPulse(Constants.ENCODER_PULSE_DISTANCE);
+        //FLEncoder.setDistancePerPulse(Constants.ENCODER_PULSE_DISTANCE);
 
     }
     
@@ -59,9 +66,9 @@ public class DriveData {
         SmartDashboard.putNumber("BL Pwr", backLeftDrive.get());
         SmartDashboard.putNumber("BR Pwr", backRightDrive.get());
 
-        SmartDashboard.putNumber("BLeft Encoder", BLEncoder.get());
-        SmartDashboard.putNumber("BRight Encoder", BREncoder.get());
-        SmartDashboard.putNumber("FLeft Encoder", FLEncoder.get());
-        SmartDashboard.putNumber("FRight Encoder", FREncoder.get());
+        SmartDashboard.putNumber("BLeft Encoder", BLEncoder.getPosition());
+        SmartDashboard.putNumber("BRight Encoder", BREncoder.getPosition());
+        SmartDashboard.putNumber("FLeft Encoder", FLEncoder.getPosition());
+        SmartDashboard.putNumber("FRight Encoder", FREncoder.getPosition());
     }
 }

78d82bc0abc12d724c108956c25e34d4b993a581
nicholas sofio <nicksofio@icloud.com>
2023-02-03 12:45:27 -0600

    adding intake to sim branch and motors

--- src/main/java/frc/robot/functions/armUtil.java
+++ src/main/java/frc/robot/functions/armUtil.java
@@ -1,5 +1,6 @@
 package frc.robot.functions;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import frc.robot.subsystems.IntakeData;
 
@@ -15,6 +16,21 @@ public class armUtil {
     public void retractAll(IntakeData pData){
         
         pData.armSolenoid.set(Value.kReverse);
+        pData.extendMotor.set(0);
+        
+
+    }
+
+    public void activate(IntakeData pData, double speed){
+
+        if (isDeployed(pData)){pData.extendMotor.set(speed);}
+        else pData.extendMotor.set(0);
+    
+    }
+
+    public boolean isDeployed(IntakeData pData){
+
+        return pData.armSolenoid.get() == DoubleSolenoid.Value.kForward;
 
     }
 
@@ -23,5 +39,7 @@ public class armUtil {
         pData.armSolenoid.set(Value.kForward);
 
     }
+
+
     
 }
--- src/main/java/frc/robot/subsystems/IntakeData.java
+++ src/main/java/frc/robot/subsystems/IntakeData.java
@@ -1,13 +1,19 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsControlModule;
+import frc.robot.Constants;
 import frc.robot.functions.telemetryUtil;
 import frc.robot.functions.telemetryUtil.Tabs;
 
 public class IntakeData {
 
     public PneumaticsControlModule pcm = new PneumaticsControlModule(0);
+
+    public final CANSparkMax extendMotor = new CANSparkMax(Constants.EXTEND_INTAKE_PORT, MotorType.kBrushless);
 
     public DoubleSolenoid armSolenoid = pcm.makeDoubleSolenoid(0, 0);
 

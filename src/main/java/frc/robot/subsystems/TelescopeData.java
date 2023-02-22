c0644a581b0f2bec145c5ba553f63933490a9ace
MinuteBots <97641018+LeoRhode@users.noreply.github.com>
2023-02-21 18:53:07 -0600

    ENCODERS

--- src/main/java/frc/robot/subsystems/TelescopeData.java
+++ src/main/java/frc/robot/subsystems/TelescopeData.java
@@ -1,9 +1,16 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.MotorCommutation;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.EncoderType;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxAlternateEncoder.Type;
 
+import edu.wpi.first.hal.EncoderJNI;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import frc.robot.constants.Hardware;
 import frc.robot.functions.telemetryUtil;
 import frc.robot.functions.telemetryUtil.Tabs;
@@ -14,8 +21,9 @@ public class TelescopeData {
     public CANSparkMax liftMotor = new CANSparkMax(Hardware.ARM_LIFT_ID, Hardware.LIFT_MOTOR_TYPE);
     public CANSparkMax retractMotor = new CANSparkMax(Hardware.ARM_RETRACT_ID, Hardware.RETRACT_MOTOR_TYPE);
 
-    public Encoder liftEncoder = new Encoder(2, 3);
-    public Encoder retractEncoder = new Encoder(0, 1);
+    public RelativeEncoder liftEncoder = liftMotor.getEncoder(com.revrobotics.SparkMaxRelativeEncoder.Type.kQuadrature, 8192);
+    public RelativeEncoder retractEncoder = retractMotor.getEncoder(com.revrobotics.SparkMaxRelativeEncoder.Type.kQuadrature, 8192);
+
 
     public DigitalInput upBound = new DigitalInput(Hardware.UP_BOUND_ID);
     public DigitalInput lowBound = new DigitalInput(Hardware.LOW_BOUND_ID);
@@ -28,8 +36,8 @@ public class TelescopeData {
         telemetryUtil.put("Lift Power", liftMotor.get(), Tabs.ROBOT);
         telemetryUtil.put("Retract Power", retractMotor.get(), Tabs.ROBOT);
 
-        telemetryUtil.put("Lift Encoder", liftEncoder.get(), Tabs.ROBOT);
-        telemetryUtil.put("Retract encoder", retractEncoder.get(), Tabs.ROBOT);
+        telemetryUtil.put("Lift Encoder", liftEncoder.getPosition(), Tabs.ROBOT);
+        telemetryUtil.put("Retract encoder", retractEncoder.getPosition(), Tabs.ROBOT);
         //telemetryUtil.put("Lift Position", liftEncoder.getPosition(), Tabs.ROBOT);
         //telemetryUtil.put("Retract Position", retractEncoder.getPosition(), Tabs.ROBOT);
 

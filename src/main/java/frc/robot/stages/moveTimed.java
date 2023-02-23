a8eae6d0c88d96d07c45f185adeef4370953d584
gavriel pappas <pappasgavriel@gmail.com>
2023-02-23 17:30:43 -0600

    working timed stages

---
+++ src/main/java/frc/robot/stages/moveTimed.java
@@ -0,0 +1,37 @@
+package frc.robot.stages;
+
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.Robot;
+import frc.robot.V2d;
+import frc.robot.functions.driveUtil;
+
+public final class moveTimed extends Stage {
+
+    Timer flymer = new Timer();
+
+    double scale;
+    double rTime;
+
+    V2d direction;
+
+
+    public moveTimed(double mscale, V2d vector, double time){
+
+        scale = mscale;
+        direction = vector;
+        rTime = time;
+
+    }
+
+
+    @Override public void init(){
+        flymer.reset();
+        flymer.start(); }
+
+
+    @Override public boolean run(Robot r) {
+        driveUtil.setPowerMechPID(r, direction.x, direction.y, scale);
+        return flymer.get() > rTime;
+    }
+
+}

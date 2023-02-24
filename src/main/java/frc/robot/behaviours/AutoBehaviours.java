cf9daf9ce449a0134a6a4f38c234a0161f00cafe
MinuteBots <97641018+LeoRhode@users.noreply.github.com>
2023-02-24 15:52:18 -0600

    idrutheroituhwruil

--- src/main/java/frc/robot/behaviours/AutoBehaviours.java
+++ src/main/java/frc/robot/behaviours/AutoBehaviours.java
@@ -34,11 +34,11 @@ public class AutoBehaviours {
             r.positionData.reInit();
             r.autoData.stages.add(new goToPosition(new V2d(1, 0)));
             r.autoData.stages.add(new timedPause(0.5));
-            r.autoData.stages.add(new goToPosition(new V2d(0, -1)));
-            r.autoData.stages.add(new timedPause(0.5));
-            r.autoData.stages.add(new goToPosition(new V2d(0, 0)));
-            r.autoData.stages.add(new timedPause(0.5));
-            r.autoData.stages.add(new goToPosition(new V2d(1, -1)));
+            // r.autoData.stages.add(new goToPosition(new V2d(0, -1)));
+            // r.autoData.stages.add(new timedPause(0.5));
+            // r.autoData.stages.add(new goToPosition(new V2d(0, 0)));
+            // r.autoData.stages.add(new timedPause(0.5));
+            // r.autoData.stages.add(new goToPosition(new V2d(1, -1)));
             };
 }
 

5e38857eada7d05eb65517b447bd504b570948b0
Gavriel Pappas <pappasgavriel@gmail.com>
2023-03-16 17:15:38 -0500

    drive stuff that might wokr

--- src/main/java/frc/robot/behaviours/subsystem/DriveBehaviors.java
+++ src/main/java/frc/robot/behaviours/subsystem/DriveBehaviors.java
@@ -14,6 +14,8 @@ import frc.robot.utils.V2d;
 
 public class DriveBehaviors {
 
+
+
     public static final Consumer<Robot> driveMech  = r -> {
 
         double x = inputUtil.deadzoneStick(r.input.driveController.getLeftX());
@@ -26,20 +28,6 @@ public class DriveBehaviors {
         driveUtil.setPowerMechanum(r.drive, x * driveScalar, y * driveScalar, z * ControlSettings.TURNING_MULT, 0.8);
     };
 
-    public static final Consumer<Robot> driveMechPID = r -> {
-
-        double x = inputUtil.deadzoneStick(r.input.driveController.getLeftX());
-        double y = inputUtil.deadzoneStick(-r.input.driveController.getLeftY());
-        double z = inputUtil.deadzoneAxis(r.input.driveController.getRightX(), ControlSettings.TURNING_DEADZONE);
-
-
-        double driveScalar = inputUtil.mapInput(
-            r.input.driveController.getRightTriggerAxis(), 1, 0, ControlSettings.MAX_DRIVE_OUT, ControlSettings.DEFAULT_DRIVE_OUT);
-
-        driveUtil.pid.target += z * ControlSettings.TURNING_SPEED * Robot.dt;
-        driveUtil.setPowerMechPID(r, x * driveScalar, y * driveScalar, 0.8);
-        telemetryUtil.put("Drive PID target", driveUtil.pid.target, Tabs.ROBOT);
-    };
 
 
 
@@ -93,36 +81,64 @@ public class DriveBehaviors {
     };
 
 
-    public static boolean PIDisActive = false;
-    public static final Consumer<Robot> driveMechDpad = r -> {
+
+
+    public static final Consumer<Robot> emmettDrive = r -> {
 
         double x = inputUtil.deadzoneStick(r.input.driveController.getLeftX());
         double y = inputUtil.deadzoneStick(-r.input.driveController.getLeftY());
         double z = inputUtil.deadzoneStick(r.input.driveController.getRightX());
 
-
         double driveScalar = inputUtil.mapInput(
             r.input.driveController.getRightTriggerAxis(), 1, 0, ControlSettings.MAX_DRIVE_OUT, ControlSettings.DEFAULT_DRIVE_OUT);
 
-        if(PIDisActive){
+        V2d flymer = new V2d(x * driveScalar, y * driveScalar);
+        flymer = flymer.rotateDegrees(r.gyro.getYaw());
 
-            V2d flymer = new V2d(x * driveScalar, y * driveScalar);
-            flymer = flymer.rotateDegrees(r.gyro.getYaw());
+        telemetryUtil.put("angle traget", driveUtil.pid.target, Tabs.DEBUG);
 
-            driveUtil.setPowerMechPID(r, flymer.x, flymer.y, 0.8);
-            if(z != 0){
-                PIDisActive = false;
-            }
-        } else{
-            driveUtil.setPowerMechanum(r.drive, x * driveScalar, y * driveScalar, z * ControlSettings.TURNING_MULT, 0.8);
-        
+        double drivePIDOut = driveUtil.pid.tick(gyroUtil.wrapAngle(r.gyro.globGyroscope.getAngle()), Robot.dt, true) + r.input.driveController.getRightY() * .18;
+
+        double pwr = drivePIDOut;
+
+        if (!(z == 0)){
+            pwr = inputUtil.mapInput(z, 1.0, -1.0, 0.6, -0.6);
+            driveUtil.pid.target = r.gyro.globGyroscope.getAngle();
         }
+
+        driveUtil.pid.target = gyroUtil.wrapAngle(driveUtil.pid.target);
+
+        if(pwr > ControlSettings.DRIVE_PID_CLAMP) { pwr = ControlSettings.DRIVE_PID_CLAMP; }
+        if(pwr < -ControlSettings.DRIVE_PID_CLAMP) { pwr = -ControlSettings.DRIVE_PID_CLAMP; }
 
         if(r.input.driveController.getPOV() != -1){
-            PIDisActive = true;
+
+            driveUtil.pid.reset();
             driveUtil.pid.target = r.input.driveController.getPOV();
+
         }
+
+        if(r.input.driveController.getRightStickButtonPressed()){
+
+            driveUtil.pid.reset();
+            driveUtil.pid.target = r.gyro.globGyroscope.getAngle()+180;
+
+        }
+
+        if (r.input.driveController.getYButtonPressed()){
+
+            r.gyro.globGyroscope.reset();
+            driveUtil.pid.reset();
+            driveUtil.pid.target = 0;
+
+        }
+
+        driveUtil.setPowerMechanum(r.drive, flymer.x, flymer.y, pwr, 0.8);
+        telemetryUtil.put("Drive PID target", driveUtil.pid.target, Tabs.ROBOT);
+
     };
+
+
 
 
     
